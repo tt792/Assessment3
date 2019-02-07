9747504a8e59f5a3c151c293c03192b7ad753d45
Jonaruu <jrhm569@york.ac.uk>
2019-02-07 10:18:15 +0000

    Revert "JOHN'S SEXY MENU"
    This reverts commit 745840499812e7bb79e8fdab9cfcd0462cd35f0c.

--- zepr/core/src/com/geeselightning/zepr/Zepr.java
+++ zepr/core/src/com/geeselightning/zepr/Zepr.java
@@ -7,7 +7,7 @@ public class Zepr extends Game {
 	private LoadingScreen loadingScreen;
 	private MenuScreen menuScreen;
 	private Level level;
-	private SelectCharacterScreen selectCharacterScreen;
+	private SelectLevelScreen selectLevelScreen;
 
 	// The progress is the integer representing the last level completed. i.e. 3 for Town
 	public static int progress = 3;
@@ -32,8 +32,8 @@ public class Zepr extends Game {
 				this.setScreen(menuScreen);
 				break;
 			case SELECT:
-				selectCharacterScreen = new SelectCharacterScreen(this);
-				this.setScreen(selectCharacterScreen);
+				selectLevelScreen = new SelectLevelScreen(this);
+				this.setScreen(selectLevelScreen);
 				break;
 			case TOWN:
 				level = new TownLevel(this);
